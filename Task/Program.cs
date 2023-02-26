56a524f8dbd41bbacb7ef1b130b7afe8e5de838d
EnotWhyNot <danik_maltsev@mail.ru>
2023-02-26 12:32:55 +0300

    commit)

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,11 +1,15 @@
-﻿
+﻿/* Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+*/
+
+
 string[] arr1 = new string[5] {"1234", "1567", "-2", "computer science", "world"};
 string[] arr2 = new string[arr1.Length];
 
 SecondArray(arr1, arr2);
 PrintArray(arr2);
 
-void SecondArray(string[] arr1, string[] arr)
+void SecondArray(string[] arr1, string[] arr2)
 {
     int count = 0;
     for (int i = 0; i < arr1.Length; i++)
@@ -17,6 +21,7 @@ void SecondArray(string[] arr1, string[] arr)
         }
     }
 }
+
 void PrintArray(string[] arr)
 {
     for (int i = 0; i < arr.Length; i++)
